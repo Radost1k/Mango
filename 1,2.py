1e5ba0903f2ee54a85c0a230062918eee6bf6e21
Radost1k <radost1k@icloud.com>
2024-10-10 15:01:25 +0000

    Iec2

---
+++ 1,2.py
@@ -0,0 +1,42 @@
+'''
+print(boopl(2))
+
+print(bool('Good'))
+
+if 1:
+    print('Hello 1')
+
+a = 3
+if a > 1:
+    print(f'Hellop {a}')
+
+b =5
+if b == 5:
+    print(f'hello{b}')
+
+a = 3
+if a > 4 :
+    print('hello 4')
+else:
+    print(f'hello {a}')
+
+a = 3
+if  a > 5:
+    print('hello 5')
+elif a < 2: 
+    print('hello 2')
+else:
+    print('hello world')    
+
+a = 3
+b = 4
+c = 5
+
+if a < 4 and b == 2:
+    print('Good')
+elif b > 3 or c ==5:
+    print('Best')
+else:
+    print('Bad')
+'''
+
---
+++ 2,2.py
@@ -0,0 +1,13 @@
+'''for i in 1 , 3 , 4:
+    print(i**2 , end = ' ')
+    
+for i in 1 , 3 , 4:
+    print(i**2 , end = '\n')
+   
+for i in 1 , 3 , 4:
+    print(i**2 , end = ' - ')
+
+a = [1 , 5 , 7 , 10]
+for i in a:
+    print(f'{i}**3 = {i**3}')
+'''
